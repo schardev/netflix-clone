6072faf355dd2b5109ccbec09a02b47d8df2ae45
Saurabh Charde <saurabhchardereal@gmail.com>
2023-02-03 00:07:01 +0530

    refactor desktop info modal a bit

--- src/components/Backdrop.tsx
+++ src/components/Backdrop.tsx
@@ -1,18 +1,21 @@
-import { motion } from "framer-motion";
+import { HTMLMotionProps, motion } from "framer-motion";
 import type { MouseEventHandler, PropsWithChildren } from "react";
 import { useEffect } from "react";
-import { j } from "../utils";
+
+type BackdropProps = PropsWithChildren<{
+  preventScroll?: boolean;
+  className?: string;
+  onClick?: MouseEventHandler<HTMLDivElement>;
+}> &
+  HTMLMotionProps<"div">;
 
 const Backdrop = ({
   preventScroll = true,
   onClick,
   className,
   children,
-}: PropsWithChildren<{
-  preventScroll?: boolean;
-  className?: string;
-  onClick?: MouseEventHandler<HTMLDivElement>;
-}>) => {
+  ...props
+}: BackdropProps) => {
   useEffect(() => {
     if (preventScroll) document.body.classList.add("prevent-scroll");
 
@@ -23,12 +26,9 @@ const Backdrop = ({
 
   return (
     <motion.div
-      className={j(className, "backdrop")}
+      className={className || "backdrop"}
       onClick={onClick}
-      initial={{ backgroundColor: "rgba(0,0,0,0)" }}
-      animate={{ backgroundColor: "rgba(0,0,0,0.4)" }}
-      exit={{ opacity: 0 }}
-      transition={{ duration: 0.2 }}>
+      {...props}>
       {children}
     </motion.div>
   );
--- src/components/DesktopInfoModal.tsx
+++ src/components/DesktopInfoModal.tsx
@@ -1,4 +1,3 @@
-import { AnimatePresence, motion, Variants } from "framer-motion";
 import {
   Check,
   NavArrowDown,
@@ -8,8 +7,7 @@ import {
   SoundOff,
   ThumbsUp,
 } from "iconoir-react";
-import { useRef, useState } from "react";
-import type ReactPlayer from "react-player";
+import { useState } from "react";
 import { useModalDispatcher } from "../contexts/ModalContext";
 import { useMyListData, useMyListDispatcher } from "../contexts/MyListProvider";
 import useFetch from "../hooks/useFetch";
@@ -17,6 +15,7 @@ import { api } from "../lib/tmdb";
 import styles from "../styles/desktop-info-modal.module.scss";
 import type {
   Credits,
+  Images,
   ListResponse,
   MovieDetails,
   TVDetails,
@@ -30,19 +29,14 @@ import YouTubeIFrame from "./YouTubeIFrame";
 
 type ExtendedRequest<T> = T & { videos: Videos } & { credits: Credits } & {
   recommendations: ListResponse<T>;
-};
+} & { images: Images };
+
 type DesktopInfoModalProps = {
   category: Exclude<Category, "person">;
   id: number;
   x: number;
   y: number;
   expanded?: boolean;
-};
-
-const modalVariants: Variants = {
-  initial: { opacity: 0, scale: 0.7 },
-  animate: { opacity: 1, scale: 1 },
-  exit: { opacity: 0, scale: 0.7 },
 };
 
 const DesktopInfoModal = ({
@@ -52,10 +46,9 @@ const DesktopInfoModal = ({
   y,
   expanded = false,
 }: DesktopInfoModalProps) => {
-  const myList = useMyListData();
+  const { isInList } = useMyListData();
   const dispatchToList = useMyListDispatcher();
   const setModalState = useModalDispatcher();
-  const playerRef = useRef<ReactPlayer>(null);
   const [muted, setIsMuted] = useState(true);
   const [isExpanded, setIsExpanded] = useState(expanded);
   const { data, isLoading } = useFetch(
@@ -65,7 +58,7 @@ const DesktopInfoModal = ({
         `${category}/${id}`,
         {
           query: {
-            append_to_response: "videos,credits,recommendations",
+            append_to_response: "videos,credits,recommendations,images",
           },
           init: { signal },
         }
@@ -81,8 +74,13 @@ const DesktopInfoModal = ({
   let numberOfSeasons = 1;
   const cast = data.credits.cast;
   const crew = data.credits.crew;
-  let starring = cast ? cast.slice(0, 4) : "TBA";
+  let starring = cast ? cast.slice(0, 4) : [];
   let director: Credits["crew"] | string = [];
+
+  let logoPath: string | null = null;
+  if (data.images?.logos && data.images.logos[0]) {
+    logoPath = data.images.logos[0].file_path!;
+  }
 
   if (category === "tv") {
     let tvShow = data as TVDetails;
@@ -103,7 +101,15 @@ const DesktopInfoModal = ({
     }
   }
   const videos = data.videos.results!.length >= 1 ? data.videos.results : null;
-  const isInList = myList.filter((item) => item.id === data.id!).length > 0;
+  const addToList = () =>
+    dispatchToList({
+      type: "add",
+      payload: {
+        id: data.id!,
+        media_type: category,
+        poster_path: api.getPosterURL(data.poster_path),
+      },
+    });
 
   if (crew) {
     if (category === "movie") {
@@ -119,77 +125,104 @@ const DesktopInfoModal = ({
     if (!director.length) director = "TBA";
   }
 
+  const details = (
+    <div
+      className={
+        isExpanded
+          ? styles["big-modal-info-details__left"]
+          : styles["mini-modal-info-details"]
+      }>
+      <h2>{title}</h2>
+      <span className={styles["span-match"]}>69% match</span>
+      {season > 1 && <span>{`${season} Seasons`}</span>}
+      {runtime && <span className={styles["span-runtime"]}>{runtime}</span>}
+      {isExpanded && data.overview && <p>{data.overview}</p>}
+    </div>
+  );
+
+  const buttons = (
+    <div
+      className={
+        isExpanded ? styles["big-player-menu"] : styles["mini-player-menu"]
+      }>
+      <button className={styles["play-btn"]}>
+        <PlayOutline />
+        {isExpanded && <span>Play</span>}
+      </button>
+      <button onClick={addToList}>
+        {isInList(data.id!, category) ? <Check /> : <Plus />}
+      </button>
+      <button>
+        <ThumbsUp />
+      </button>
+      {!isExpanded && (
+        <button onClick={() => setIsExpanded(true)}>
+          <NavArrowDown />
+        </button>
+      )}
+    </div>
+  );
+
   return (
-    <AnimatePresence>
-      {!isExpanded ? (
-        <motion.div
-          variants={modalVariants}
-          initial="initial"
-          animate="animate"
-          exit="exit"
-          transition={{ type: "tween", duration: 0.2 }}
-          style={{ top: y, left: x }}
-          className={styles["mini-modal-container"]}
-          // onPointerLeave={() => setModalState({ visible: false })}>
-        >
-          {videos ? (
-            <div className={styles["player"]}>
-              <YouTubeIFrame
-                ref={playerRef}
-                videoKey={videos[0].key!}
-                playing={true}
-                controls={true}
-                muted={muted}
-                loop={true}
-                onError={() => {
-                  console.log("error playing video");
-                }}
-              />
-              <div className={styles["player-controls"]}>
-                <button
-                  className={styles["mute-btn"]}
-                  onClick={() => setIsMuted(!muted)}>
-                  {muted ? <SoundOff /> : <SoundHigh />}
-                </button>
+    <Backdrop
+      layout
+      initial={{ opacity: 0.5, scale: 0.7, backgroundColor: "rgba(0,0,0,0)" }}
+      animate={{
+        opacity: 1,
+        scale: 1,
+        ...(isExpanded
+          ? { backgroundColor: "rgba(0,0,0,0.7)", transition: { delay: 0.3 } }
+          : {}),
+      }}
+      transition={{ type: "tween", duration: 0.3 }}
+      exit={{ opacity: 0 }}
+      style={!isExpanded ? { top: y, left: x } : {}}
+      preventScroll={isExpanded}
+      onClick={(e) => {
+        if (e.currentTarget === e.target && isExpanded) {
+          setModalState({ visible: false });
+        }
+      }}
+      className={
+        !isExpanded ? styles["mini-motion-modal"] : styles["big-motion-modal"]
+      }>
+      <div
+        className={
+          isExpanded
+            ? styles["big-modal-container"]
+            : styles["mini-modal-container"]
+        }>
+        {videos ? (
+          <div className={styles["player"]}>
+            <YouTubeIFrame
+              videoKey={videos[0].key!}
+              playing={true}
+              controls={true}
+              muted={muted}
+              loop={true}
+            />
+            {isExpanded && logoPath && (
+              <div className={styles.logo}>
+                <img src={api.getLogoURL(logoPath)} alt="" />
               </div>
-            </div>
-          ) : (
-            <img src={api.getBackdropURL(data.backdrop_path)} alt="" />
-          )}
-          <div className={styles["mini-modal-info-container"]}>
-            <div className={styles["mini-modal-menu"]}>
-              <button className={styles["play-btn"]}>
-                <PlayOutline />
-              </button>
+            )}
+            <div className={styles["player-controls"]}>
+              {isExpanded && buttons}
               <button
-                onClick={() => {
-                  dispatchToList({
-                    type: "add",
-                    payload: {
-                      id: data.id!,
-                      media_type: category,
-                      poster_path: api.getPosterURL(data.poster_path),
-                    },
-                  });
-                }}>
-                {isInList ? <Check /> : <Plus />}
-              </button>
-              <button>
-                <ThumbsUp />
-              </button>
-              <button onClick={() => setIsExpanded(true)}>
-                <NavArrowDown />
+                className={styles["mute-btn"]}
+                onClick={() => setIsMuted(!muted)}>
+                {muted ? <SoundOff /> : <SoundHigh />}
               </button>
             </div>
-            <div className={styles["details"]}>
-              <h2>{title}</h2>
-              <span className={styles["span-match"]}>69% match</span>
-              {season > 1 && <span>{`${season} Seasons`}</span>}
-              {runtime && (
-                <span className={styles["span-runtime"]}>{runtime}</span>
-              )}
-            </div>
-            <div className={styles["genres"]}>
+          </div>
+        ) : (
+          <img src={api.getBackdropURL(data.backdrop_path)} alt="" />
+        )}
+        {!isExpanded ? (
+          <div className={styles["mini-modal-info-container"]}>
+            {buttons}
+            {details}
+            <div className={styles["genre-list"]}>
               {data.genres?.map((genre) => (
                 <span key={genre.id}>
                   <span>{genre.name}</span>
@@ -197,154 +230,16 @@ const DesktopInfoModal = ({
               ))}
             </div>
           </div>
-        </motion.div>
-      ) : (
-        <Backdrop
-          className={styles.backdrop}
-          onClick={(e) => {
-            if (e.currentTarget === e.target) {
-              setModalState({ visible: false });
-            }
-          }}>
-          <div className={styles["big-modal-container"]}>
-            {videos ? (
-              <div className={styles["player"]}>
-                <YouTubeIFrame
-                  ref={playerRef}
-                  videoKey={videos[0].key!}
-                  playing={true}
-                  controls={true}
-                  muted={muted}
-                  loop={true}
-                  onError={() => {
-                    console.log("error playing video");
-                  }}
-                />
-                <div className={styles["player-controls"]}>
-                  <div className={styles["player-menu"]}>
-                    <button className={styles["play-btn"]}>
-                      <PlayOutline />
-                      <span>Play</span>
-                    </button>
-                    <button
-                      onClick={() => {
-                        dispatchToList({
-                          type: "add",
-                          payload: {
-                            id: data.id!,
-                            media_type: category,
-                            poster_path: api.getPosterURL(data.poster_path),
-                          },
-                        });
-                      }}>
-                      {isInList ? <Check /> : <Plus />}
-                    </button>
-                    <button>
-                      <ThumbsUp />
-                    </button>
-                  </div>
-                  <button
-                    className={styles["mute-btn"]}
-                    onClick={() => setIsMuted(!muted)}>
-                    {muted ? <SoundOff /> : <SoundHigh />}
-                  </button>
-                </div>
-              </div>
-            ) : (
-              <img src={api.getBackdropURL(data.backdrop_path)} alt="" />
-            )}
-            <div className={styles["big-modal-info-container"]}>
-              <div className={styles["big-modal-info-details"]}>
-                <div className={styles["big-modal-info-details__left"]}>
-                  <h2>{title}</h2>
-                  <span className={styles["span-match"]}>69% match</span>
-                  {season > 1 && <span>{`${season} Seasons`}</span>}
-                  {runtime && (
-                    <span className={styles["span-runtime"]}>{runtime}</span>
-                  )}
-                  {data.overview && <p>{data.overview}</p>}
-                </div>
-                <div className={styles["big-modal-info-details__right"]}>
-                  {Array.isArray(starring) && (
-                    <div>
-                      <span className={styles.title}>Cast: </span>
-                      {starring.map((star, idx) => {
-                        return (
-                          <span>
-                            {star.name}
-                            {starring.length - 1 !== idx && ", "}
-                          </span>
-                        );
-                      })}
-                    </div>
-                  )}
-                  {/* // TODO simplify checks */}
-                  {Array.isArray(data.genres) && (
-                    <div>
-                      <span className={styles.title}>Genre: </span>
-                      {data.genres.map((genre, idx) => (
-                        <span>
-                          {genre.name}
-                          {data.genres!.length - 1 !== idx && ", "}
-                        </span>
-                      ))}
-                    </div>
-                  )}
-                </div>
-              </div>
-              <div className={styles["sections"]}>
-                <div className={styles["episodes-section"]}>
-                  <EpisodeSelector
-                    id={+id}
-                    seasons={numberOfSeasons}
-                    showHeading={true}
-                  />
-                </div>
-                <div className={styles["recommendation-section"]}>
-                  <h2>More Like This</h2>
-                  <Slider
-                    endpoint={`${category}/${id}/recommendations`}
-                    flow="row"
-                  />
-                </div>
-                {videos && (
-                  <div className={styles["trailers-section"]}>
-                    <h2>Trailer & More</h2>
-                    <div className={styles["video-frames"]}>
-                      {videos.map((video) => {
-                        return (
-                          <VideoFrame
-                            key={video.key!}
-                            videoKey={video.key!}
-                            title={video.name!}
-                          />
-                        );
-                      })}
-                    </div>
-                  </div>
-                )}
-              </div>
-              <div className={styles["big-modal-info-about"]}>
-                <h2>
-                  About <span>{title}</span>
-                </h2>
-                {Array.isArray(director) && (
+        ) : (
+          <div className={styles["big-modal-info-container"]}>
+            <div className={styles["big-modal-info-details"]}>
+              {details}
+              <div className={styles["big-modal-info-details__right"]}>
+                {/* // TODO simplify checks */}
+                {Array.isArray(starring) && starring.length > 0 && (
                   <div>
-                    <span className={styles.title}>
-                      {category === "tv" ? "Creators: " : "Director: "}
-                    </span>
-                    {director.map((crew, idx) => (
-                      <span>
-                        {crew.name}
-                        {director!.length - 1 !== idx && ", "}
-                      </span>
-                    ))}
-                  </div>
-                )}
-                {Array.isArray(cast) && (
-                  <div>
-                    <span className={styles.title}>Cast: </span>
-                    {cast.map((star, idx) => {
+                    <span className={styles["span-title"]}>Cast: </span>
+                    {starring.map((star, idx) => {
                       return (
                         <span>
                           {star.name}
@@ -354,10 +249,9 @@ const DesktopInfoModal = ({
                     })}
                   </div>
                 )}
-                {/* // TODO simplify checks */}
                 {Array.isArray(data.genres) && (
                   <div>
-                    <span className={styles.title}>Genre: </span>
+                    <span className={styles["span-title"]}>Genre: </span>
                     {data.genres.map((genre, idx) => (
                       <span>
                         {genre.name}
@@ -368,10 +262,86 @@ const DesktopInfoModal = ({
                 )}
               </div>
             </div>
+            <div className={styles["sections"]}>
+              <div className={styles["episodes-section"]}>
+                <EpisodeSelector
+                  id={+id}
+                  seasons={numberOfSeasons}
+                  showHeading={true}
+                />
+              </div>
+              {data.recommendations.results!.length > 0 && (
+                <div className={styles["recommendation-section"]}>
+                  <h2>More Like This</h2>
+                  <Slider
+                    endpoint={`${category}/${id}/recommendations`}
+                    flow="row"
+                  />
+                </div>
+              )}
+              {videos && (
+                <div className={styles["trailers-section"]}>
+                  <h2>Trailer & More</h2>
+                  <div className={styles["video-frames"]}>
+                    {videos.map((video) => {
+                      return (
+                        <VideoFrame
+                          key={video.key!}
+                          videoKey={video.key!}
+                          title={video.name!}
+                        />
+                      );
+                    })}
+                  </div>
+                </div>
+              )}
+            </div>
+            <div className={styles["big-modal-info-about"]}>
+              <h2>
+                About <span>{title}</span>
+              </h2>
+              {Array.isArray(director) && (
+                <div>
+                  <span className={styles["span-title"]}>
+                    {category === "tv" ? "Creators: " : "Director: "}
+                  </span>
+                  {director.map((crew, idx) => (
+                    <span>
+                      {crew.name}
+                      {director!.length - 1 !== idx && ", "}
+                    </span>
+                  ))}
+                </div>
+              )}
+              {Array.isArray(cast) && (
+                <div>
+                  <span className={styles["span-title"]}>Cast: </span>
+                  {cast.map((star, idx) => {
+                    return (
+                      <span>
+                        {star.name}
+                        {starring.length - 1 !== idx && ", "}
+                      </span>
+                    );
+                  })}
+                </div>
+              )}
+              {Array.isArray(data.genres) && (
+                <div>
+                  <span className={styles["span-title"]}>Genre: </span>
+                  {data.genres.map((genre, idx) => (
+                    <span>
+                      {genre.name}
+                      {data.genres!.length - 1 !== idx && ", "}
+                    </span>
+                  ))}
+                </div>
+              )}
+            </div>
           </div>
-        </Backdrop>
-      )}
-    </AnimatePresence>
+        )}
+      </div>
+    </Backdrop>
   );
 };
 
